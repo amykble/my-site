640c5a37a3daf27ce5f9e6d68bcae9e0f7a118ab
amykble <56142077+amykble@users.noreply.github.com>
2020-08-09 18:32:01 +0100

    Updating project cards

--- src/components/modules/RecentProjects.vue
+++ src/components/modules/RecentProjects.vue
@@ -8,29 +8,41 @@
       </header>
       <section class="mx-6 mt-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 lg:gap-10">
         <article v-for="edge in $page.portfolio.edges" :key="edge.node.id" class="z-10 relative">
-          <g-link :to="edge.node.path" class="group bg-hover">
+          <div class="group bg-hover">
             <div
-              class="p-6 h-full flex flex-col justify-between bg-white hover:bg-blue-100 transition-all duration-200 ease-in-out"
+              class="p-6 h-full bg-white hover:bg-blue-100 transition-all duration-200 ease-in-out"
             >
-              <div>
-                <g-image :src="edge.node.img" class="h-40 w-auto mx-auto" />
-              </div>
-              <div>
-                <h2 class="mt-4 text-2xl text-blue-600">{{ edge.node.title }}</h2>
-              </div>
+              <g-link
+                g-link
+                :to="edge.node.path"
+                class="text-blue-600 hover:cursor-pointer focus:cursor-pointer"
+              >
+                <div>
+                  <g-image :src="edge.node.img" class="h-32 w-auto mx-auto" />
+                </div>
+                <div>
+                  <h2 class="mt-4 text-2xl">{{ edge.node.title }}</h2>
+                </div>
+              </g-link>
               <div class="mt-4 flex">
-                <a :href="edge.node.github">
+                <a
+                  :href="edge.node.github"
+                  class="opacity-60 hover:opacity-100 focus:opacity-100 transition duration-200 ease-in-out"
+                >
                   <img
                     src="../../assets/images/icons8-github.svg"
                     alt="github logo"
-                    class="h-12 w-auto"
+                    class="h-8 w-auto"
                   />
                 </a>
-                <a :href="edge.node.website">
+                <a
+                  :href="edge.node.website"
+                  class="opacity-60 hover:opacity-100 focus:opacity-100 transition duration-200 ease-in-out"
+                >
                   <img
                     src="../../assets/images/icons8-website-96.png"
                     alt="internet icon"
-                    class="h-12 w-auto ml-4"
+                    class="h-8 w-auto ml-4"
                   />
                 </a>
               </div>
@@ -38,7 +50,7 @@
                 <h3 class="mt-4 italic text-gray-800">{{ edge.node.date }}</h3>
               </div>
             </div>
-          </g-link>
+          </div>
         </article>
         <article class="lg:hidden z-10 relative">
           <g-link to="/blog" class="group bg-hover">
